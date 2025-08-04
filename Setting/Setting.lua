02ed990cfaa24f7ec57fab5f31afa38c6be6b4d6
123fa98 <123fa98vip@gmail.com>
2025-08-04 20:51:40 +0800

    Create Setting.lua

---
+++ Setting/Setting.lua
@@ -0,0 +1,18 @@
+local Notice = {
+    {Date = "Year-Months-Day", Text = "Text", ImageUrl = ""},
+}
+local LoadServer = {
+    "最强战场",
+    "Blox Fruit",
+    "疯狂之城",
+    "忍者传奇",
+    "俄亥俄州",
+    "刀刃球",
+    "战争大亨",
+    "Fisch",
+    "Doors",
+}
+local Sponsors = {
+    {QQ = "3944435268", Amount = 100},
+}
+return Notice, LoadServer, Sponsors
