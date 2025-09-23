2a874b71b2c3bc3afa907eb14646414cc0727244
123fa98 <123fa98vip@gmail.com>
2025-09-23 13:22:02 +0800

    Update 汉化中心.lua

--- 汉化中心.lua
+++ 汉化中心.lua
@@ -10,12 +10,13 @@ local Tab = Window:MakeTab({
     Title = "主要",
     Icon = "home"
 })
-
+Tab:AddSection("以下内容均为Xi Pro付费内容，需要卡密才可使用")
 for Name, Link in List do
     Tab:AddButton({
         Name = Name, 
         Callback = function()
-            loadstring(Link)()
+            loadstring(game:HttpGet(Link))()
         end
     })
 end
+
